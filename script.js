419e8ea21832cdf552b3c66a84111b8930e3df33
WallasBritoo <wallasbritoporto@gmail.com>
2024-02-14 21:24:23 -0300

    Caracter

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 function pswGenerate(){
     let characters = 'abcdefghijklmnopqrstuvwxyz';
-    let password = '';
-    let number = '1234567890';
-    let upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let special = '!@#$%¨&*()_+{^}`>:<?';
-
+    let password = '';  
     let legth = document.getElementById('pswLeght').value;   
     let qtd = document.getElementById('pswQtd').value;
     let spc = document.getElementById('special').checked;
     let numb = document.getElementById('number').checked;
     let upp = document.getElementById('upperCase').checked;
 
-    password = password.concat(number);
-    password = password.concat(upperCase);
+    if(spc == true){characters += '!@#$%¨&*()_+{^}`>:<?';}
+    if(numb == true){characters +='1234567890';}
+    if(upp == true){characters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';}
 
     
     for(let y = 1; y <= legth; y++ ){
         password = password + characters.charAt(Math.floor(Math.random() * characters.length));
     }
     alert(password);
+
+
+
 }
 
 function pswClean(){
