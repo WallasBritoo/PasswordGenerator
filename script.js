6e6ef25668ebd6dd54997f70722b415e56f2ffb0
WallasBritoo <wallasbritoporto@gmail.com>
2024-02-14 21:01:28 -0300

    att

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 function pswGenerate(){
-
+    let characters = 'abcdefghijklmnopqrstuvwxyz';
     let password = '';
+    let number = '1234567890';
+    let upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let special = '!@#$%¨&*()_+{^}`>:<?';
+
     let legth = document.getElementById('pswLeght').value;   
     let qtd = document.getElementById('pswQtd').value;
     let spc = document.getElementById('special').checked;
     let numb = document.getElementById('number').checked;
     let upp = document.getElementById('upperCase').checked;
+
+    password = password.concat(number);
+    password = password.concat(upperCase);
+
     
     for(let y = 1; y <= legth; y++ ){
-        password = password + 'a';
+        password = password + characters.charAt(Math.floor(Math.random() * characters.length));
     }
     alert(password);
 }
